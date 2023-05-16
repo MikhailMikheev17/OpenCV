798f9dfb858d1af59511d7eee47c3ef58a7dd5a6
MikhailMikheev <Mikhail-Mikheev-7@yandex.ru>
2023-05-16 15:25:18 +0300

    update main

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import cv2
 
 
-img = 
+img = cv2.imread('test.jpg')# считывание изображения, в '' можно указать адрес изображения
+#print(img.shape)
+img2 = cv2.resize(img,(500,500)) # изменение размера
+#print(img2.shape)
+
+cv2.imshow('Result', img2) # открытие изображения
+cv2.waitKey(0)# пауза бесконечная
